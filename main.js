e1f2f28cb03c057041552967c267b11ccc33807e
@douglasabnovato <douglasabnovato@gmail.com>
2019-04-01 19:02:33 -0300

    #7 arrow function

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-class List{
-    constructor(){
-        this.data = [];
-    }
+const arr = [1,3,4,5,8,9];
 
-    add(data){
-        this.data.push(data);
-    }
-}
+const newArr = arr.map(function(item,index){
+    return item * index;
+});
 
-class TodoList extends List{
-    
-}
+console.log(newArr);
 
-const MinhaLista = new TodoList();
+const sum = arr.reduce(function(total, next){
+    return total + next;
+});
 
-document.getElementById('novotodo').onclick = function(){
-    MinhaLista.add();
-}
+console.log(sum);
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0;    
+});
+
+console.log(filter);
+
+const find = arr.find(function(item){
+    return item === 3;
+});
+
+console.log(find);
