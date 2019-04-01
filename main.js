6d13e9392add4b52e27742f74d0716f113ddc5e6
@douglasabnovato <douglasabnovato@gmail.com>
2019-04-01 18:14:49 -0300

    #4 classes

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert('teste');
+class List{
+    constructor(){
+        this.data = [];
+    }
+
+    add(data){
+        this.data.push(data);
+    }
+}
+
+class TodoList extends List{
+    
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add();
+}
