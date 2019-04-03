b5902c66d1960683d8a64eb411d2a0bd5d917987
@douglasabnovato <douglasabnovato@gmail.com>
2019-04-03 16:08:08 -0300

    #atualizando

---
+++ main3.js
@@ -0,0 +1,20 @@
+// rest 
+
+const usuario = {
+    nome : 'Diego',
+    idade : 23,
+    empresa : 'Rocketseat'
+};
+
+const { nome, ... resto } = usuario;
+
+console.log(nome);
+console.log(resto);
+
+const arr = [1,2,3,4];
+
+const [a,b, ... c] = arr;
+
+console.log(a);
+console.log(b);
+console.log(c);
