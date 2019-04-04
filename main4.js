68ce0ce9ea9a4ca267fe5cc8aa66ebf63d7ccbdc
@douglasabnovato <douglasabnovato@gmail.com>
2019-04-04 18:43:31 -0300

    #revisando

---
+++ main4.js
@@ -0,0 +1,9 @@
+/**
+ * não é permitido : 
+ * const a = 1; 
+ * a = 3;
+ * mutação de uma const :
+ * */
+const usuario = {nome : 'Douglas'};
+usuario.nome = 'Diego';
+console.log(usuario);
---
+++ main5.js
@@ -0,0 +1,10 @@
+function teste(x){
+    let y = 2;
+    if (x > 5) {
+        console.log(x,y);
+    }
+}
+
+console.log(y);
+
+teste(10);
