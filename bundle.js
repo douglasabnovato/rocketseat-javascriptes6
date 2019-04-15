af760fe1c7af80eba483b4cbdb43d9ac1cf47d26
@douglasabnovato <douglasabnovato@gmail.com>
2019-04-15 18:21:27 -0300

    #1 desafio

--- bundle.js
+++ bundle.js
@@ -1,82 +1,77 @@
 "use strict";
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-// dado o vetor :
-var usuarios = [{
-  nome: 'Diego',
-  idade: 23,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Gabriel',
-  idade: 15,
-  empresa: 'Rocketseat'
-}, {
-  nome: 'Lucas',
-  idade: 30,
-  empresa: 'Facebook'
-}, {
-  nome: 'Danilo',
-  idade: 25,
-  empresa: 'Rocketseat'
-}]; //utilizar os métodos de array (map, reduce, filter e find)
+//converter o código em Arrow Functions
 
 /**
- * 2.1 Utilizando o map : Crie uma variável que contenha todas idades dos usuários: 
- * Resultado : [23, 15, 30]
+ * 3.1
+ * const arr = [1, 2, 3, 4, 5];
+ * arr.map(function(item) {
+ *      return item + 10;
+ * });
+ */
+var arr = [1, 2, 3, 4, 5];
+console.log(arr);
+var soma10 = arr.map(function (ar) {
+  return ar + 10;
+});
+console.log(soma10);
+/**
+ * 3.2
+ * Dica: Utilize uma constante pra function
+ * const usuario = { nome: 'Diego', idade: 23 };
+ * function mostraIdade(usuario) {
+ *      return usuario.idade;
+ * }
+ * mostraIdade(usuario);
  */
 
-var idades = usuarios.map(function (usuario) {
+var usuario = {
+  nome: "Douglas",
+  idade: 29
+};
+
+var mostrarIdade = function mostrarIdade(usuario) {
   return usuario.idade;
-});
-console.log(idades); //extras
+};
 
-var empresas = usuarios.map(function (usuario) {
-  return usuario.empresa;
-});
-console.log(empresas);
-var nomes = usuarios.map(function (usuario) {
-  return usuario.nome;
-});
-console.log(nomes);
+console.log(mostrarIdade(usuario));
 /**
- * 2.2 Utilizando o filter :  
- * Crie uma variáveis que tenha apenas os usuários que trabalham na Rocketseat e com mais de 18 anos:
- * Resultado : [{ nome: 'Diego', idade: 23, empresa: 'Rocketseat' }]
+ * 3.3
+ * Dica: Utilize uma constante pra function
+ * const nome = "Diego";
+ * const idade = 23;
+ * function mostraUsuario(nome = 'Diego', idade = 18) {
+ *      return { nome, idade };
+ * }
+ * mostraUsuario(nome, idade);
+ * mostraUsuario(nome);
  */
 
-var rocketseat18 = usuarios.filter(function (usuario) {
-  return usuario.empresa === 'Rocketseat' && usuario.idade >= 18;
-});
-console.log(rocketseat18);
+var nome = "Douglas";
+var idade = 23;
+
+var mostrarUsuario = function mostrarUsuario() {
+  var nome = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Diego";
+  var idade = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 18;
+  return {
+    nome: nome,
+    idade: idade
+  };
+};
+
+console.log(mostrarUsuario(nome, idade));
+console.log(mostrarUsuario(nome));
 /**
- * 2.3 Utilizando o find : 
- * Crie uma variável que procura por um usuário que trabalhe na empresa Google:
- * Resultado : undefined
+ * 3.4
+ * const promise = function() {
+ *      return new Promise(function(resolve, reject) {
+ *          return resolve();
+ *      })
+ * }
  */
 
-var salarioGoogle = usuarios.find(function (usuario) {
-  return usuario.empresa === 'Google';
-});
-console.log(salarioGoogle);
-/**
- * 2.4 Unindo operações :
- * Multiplique a idade de todos usuários por dois e depois realize um filtro nos usuários que possuem 
- * no máximo 50 anos:
- * Resultado:
- * [
- *  { nome: 'Diego', idade: 46, empresa: 'Rocketseat' },
- *  { nome: 'Gabriel', idade: 30, empresa: 'Rocketseat' },
- * ]
- */
-
-var calculo = usuarios.map(function (usuario) {
-  return _objectSpread({}, usuario, {
-    idade: usuario.idade * 2
+var promise = function promise() {
+  return new Promise(function (resolve, reject) {
+    return resolve();
   });
-}).filter(function (usuario) {
-  return usuario.idade <= 50;
-});
-console.log(calculo);
+};
