ea089e7ab22f7aa2eddffc6120015cd9357f6f14
@douglasabnovato <douglasabnovato@gmail.com>
2019-04-16 16:59:31 -0300

    desafio 1 ex 6

--- main.js
+++
@@ -1,7 +0,0 @@
-/**
- * 6
- * Converta o seguinte trecho de código utilizando Template Literals:
- * const usuario = 'Diego';
- * const idade = 23;
- * console.log('O usuário ' + usuario + ' possui ' + idade + ' anos');
- */
